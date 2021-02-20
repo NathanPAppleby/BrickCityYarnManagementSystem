88dd8e3219599193a5fb92fad91235471c918b8d
Ella <esc2570@rit.edu>
2021-02-20 15:14:48 -0500

    added arraylist for projects, added methods for adding and removing from arraylist, added method to change state

--- src/Project.java
+++ src/Project.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @author Nathan Appleby
  * @author Zackary Wake
@@ -18,6 +20,42 @@ public class Project {
         this.status = status;
         this.pattern = pattern;
         this.yarn = yarn;
+    }
+
+    public ArrayList<Project> projectList = new ArrayList<>();
+
+    /**
+     * Adds project to arraylist
+     * @param p project to be added
+     */
+    public void addProject(Project p){
+        projectList.add(p);
+    }
+
+    /**
+     * Removes project from arraylist (not moving queue, removes from all queues totally)
+     * @param p project to be deleted
+     */
+    public void removeProject(Project p){
+        projectList.remove(p);
+    }
+
+    /**
+     * Changes the status of the project.
+     * All error checking for state is done before method is called
+     * @param p the project to be moved
+     * @param status the desired state
+     */
+    public void changeProjectStatus(Project p, State status){
+        if(status == State.inQueue){
+            p.status = State.inQueue;
+        }
+        else if(status == State.WIP){
+            p.status = State.WIP;
+        }
+        else if(status == State.complete){
+            p.status = State.complete;
+        }
     }
 
     public State getStatus() {
