8f272d680c3867b926727a5ae9a382966c9ca213
Ella Cronk <El918088Cr@dmtllc.org>
2022-01-26 16:02:24 -0500

    initialized, set up login

---
+++ src_c#/YMS_CSharp/YMS_CSharp/LoginForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace YMS_CSharp
+{
+  partial class LoginForm
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if(disposing&&(components!=null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.UsernameLoginEdit = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.PasswordLoginEdit = new System.Windows.Forms.TextBox();
+      this.LoginBtn = new System.Windows.Forms.Button();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.label7 = new System.Windows.Forms.Label();
+      this.UsernameSignUpEdit = new System.Windows.Forms.TextBox();
+      this.PasswordSignUpEdit = new System.Windows.Forms.TextBox();
+      this.PreferredNameSignUpEdit = new System.Windows.Forms.TextBox();
+      this.SignUpBtn = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(39, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(80, 32);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Log In";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label2.Location = new System.Drawing.Point(270, 9);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(98, 32);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Sign Up";
+      // 
+      // UsernameLoginEdit
+      // 
+      this.UsernameLoginEdit.Location = new System.Drawing.Point(78, 57);
+      this.UsernameLoginEdit.Name = "UsernameLoginEdit";
+      this.UsernameLoginEdit.Size = new System.Drawing.Size(100, 23);
+      this.UsernameLoginEdit.TabIndex = 3;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 60);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(60, 15);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Username";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(12, 91);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(57, 15);
+      this.label4.TabIndex = 5;
+      this.label4.Text = "Password";
+      // 
+      // PasswordLoginEdit
+      // 
+      this.PasswordLoginEdit.Location = new System.Drawing.Point(78, 91);
+      this.PasswordLoginEdit.Name = "PasswordLoginEdit";
+      this.PasswordLoginEdit.Size = new System.Drawing.Size(100, 23);
+      this.PasswordLoginEdit.TabIndex = 6;
+      // 
+      // LoginBtn
+      // 
+      this.LoginBtn.Location = new System.Drawing.Point(44, 130);
+      this.LoginBtn.Name = "LoginBtn";
+      this.LoginBtn.Size = new System.Drawing.Size(75, 23);
+      this.LoginBtn.TabIndex = 7;
+      this.LoginBtn.Text = "Log In";
+      this.LoginBtn.UseVisualStyleBackColor = true;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(230, 57);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(60, 15);
+      this.label5.TabIndex = 8;
+      this.label5.Text = "Username";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(230, 82);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(57, 15);
+      this.label6.TabIndex = 9;
+      this.label6.Text = "Password";
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(200, 106);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(90, 15);
+      this.label7.TabIndex = 10;
+      this.label7.Text = "Preferred Name";
+      // 
+      // UsernameSignUpEdit
+      // 
+      this.UsernameSignUpEdit.Location = new System.Drawing.Point(305, 54);
+      this.UsernameSignUpEdit.Name = "UsernameSignUpEdit";
+      this.UsernameSignUpEdit.Size = new System.Drawing.Size(100, 23);
+      this.UsernameSignUpEdit.TabIndex = 11;
+      // 
+      // PasswordSignUpEdit
+      // 
+      this.PasswordSignUpEdit.Location = new System.Drawing.Point(305, 79);
+      this.PasswordSignUpEdit.Name = "PasswordSignUpEdit";
+      this.PasswordSignUpEdit.Size = new System.Drawing.Size(100, 23);
+      this.PasswordSignUpEdit.TabIndex = 12;
+      // 
+      // PreferredNameSignUpEdit
+      // 
+      this.PreferredNameSignUpEdit.Location = new System.Drawing.Point(305, 103);
+      this.PreferredNameSignUpEdit.Name = "PreferredNameSignUpEdit";
+      this.PreferredNameSignUpEdit.Size = new System.Drawing.Size(100, 23);
+      this.PreferredNameSignUpEdit.TabIndex = 13;
+      // 
+      // SignUpBtn
+      // 
+      this.SignUpBtn.Location = new System.Drawing.Point(279, 132);
+      this.SignUpBtn.Name = "SignUpBtn";
+      this.SignUpBtn.Size = new System.Drawing.Size(75, 23);
+      this.SignUpBtn.TabIndex = 14;
+      this.SignUpBtn.Text = "Sign Up";
+      this.SignUpBtn.UseVisualStyleBackColor = true;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(429, 181);
+      this.Controls.Add(this.SignUpBtn);
+      this.Controls.Add(this.PreferredNameSignUpEdit);
+      this.Controls.Add(this.PasswordSignUpEdit);
+      this.Controls.Add(this.UsernameSignUpEdit);
+      this.Controls.Add(this.label7);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.LoginBtn);
+      this.Controls.Add(this.PasswordLoginEdit);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.UsernameLoginEdit);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Name = "MainForm";
+      this.Text = "Yarn Management System";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox UsernameLoginEdit;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox PasswordLoginEdit;
+    private System.Windows.Forms.Button LoginBtn;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.TextBox UsernameSignUpEdit;
+    private System.Windows.Forms.TextBox PasswordSignUpEdit;
+    private System.Windows.Forms.TextBox PreferredNameSignUpEdit;
+    private System.Windows.Forms.Button SignUpBtn;
+  }
+}
+
